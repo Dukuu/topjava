3ea761e69155760649cfec66a7b4523971b75a05
Dukuu <alezin7@gmail.com>
2017-09-12 18:27:13 +0300

    11 final fix

--- src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 //  http://stackoverflow.com/a/22358422/548473
 @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason = "No data found")  // 422
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends ApplicationException {
+    private static final String EXCEPTION_COMMON_NOT_FOUND = "exception.common.notFound";
+
+    //  http://stackoverflow.com/a/22358422/548473
+    public NotFoundException(String arg) {
+        super(EXCEPTION_COMMON_NOT_FOUND, HttpStatus.UNPROCESSABLE_ENTITY, arg);
     }
 }
