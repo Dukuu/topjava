a1f9acd4a2afe6528174d023b8dbdbf59acd0c1e
Dukuu <alezin7@gmail.com>
2017-10-17 11:16:28 +0300

    11 07 XSS

--- src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 //  http://stackoverflow.com/a/22358422/548473
 @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason = "No data found")  // 422
-import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends ApplicationException {
     private static final String EXCEPTION_COMMON_NOT_FOUND = "exception.common.notFound";
